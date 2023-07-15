e1d2c13b28243593bc8b373e14cd6444a527c1bc
VODKA312 <15356203871@163.com>
2023-07-15 15:53:45 +1000

    data process for group_use_technique

--- test_data_prepare.py
+++ test_data_prepare.py
@@ -1,29 +1,57 @@
 import pandas as pd
+# 读取整个 Excel 文件
+xls = pd.ExcelFile("./DataSource/new_attck_relationship.xlsx")
+# 获取所有子表的名称
+sheet_names = xls.sheet_names
+#先获取technique子表中所有的technique信息
+selected_sheet = sheet_names[1]  # 假设选择第一个子表
 
-# 读取数据文件
-data = pd.read_excel("./DataSource/test.xlsx")  # 替换为你的数据文件路径
-# 总的技术种类列
-total_techniques = data.columns[3:]  # 假设总的技术种类列从第4列开始
+# 读取选择的子表数据
+df_technique = pd.read_excel(xls, selected_sheet)
+# 获取 ID 列的所有数据并放入列表
+id_list = df_technique['ID'].tolist()
+# 选择要读取的特定子表——group_use_technique
+selected_sheet = sheet_names[0]  # 假设选择第一个子表
+# 读取选择的子表数据
+df_group_technique = pd.read_excel(xls, selected_sheet)
+# 创建一个空字典用于构建JSON数据
+json_data = {}
 
-print(total_techniques)
-# 遍历每一行数据
-for index, row in data.iterrows():
-    # 获取当前行的target id，是单个变量
-    target_id = row["target ID"]
-    # 设置索引
-    index_column = 0
-    # 遍历总的技术种类列
-    for column in total_techniques:
-        # 判断target id下的技术种类是否存在于总的技术种类列中
-        if target_id in column:
-            # 在存在的技术种类列中填充1
-            # print(data.iloc[index, index_column+3])
-            data.iloc[index, index_column+3] = 1
-        else:
-            # 在不存在的技术种类列中填充0
-            # print(data.iloc[index, index_column+3])
-            data.iloc[index, index_column+3] = 0
-        index_column = index_column + 1
+# 遍历每个group
+for group in df_group_technique['source ID'].unique():
+    # 获取当前group的数据
+    group_data = df_group_technique[data['source ID'] == group]
 
-# 保存修改后的数据
-data.to_excel("./DataSource/test_3.xlsx", index=False)  # 替换为你想保存的文件路径，index=False表示不保存索引
+    # 构建group的技术列表
+    techniques = group_data['target ID'].tolist()
+
+    # 将group和对应的技术列表添加到JSON字典中
+    json_data[group] = techniques
+
+# 将JSON字典转换为JSON字符串
+json_str = pd.io.json.dumps(json_data, indent=4)
+
+# 打印JSON字符串
+print(json_str)
+
+# 创建一个空的DataFrame作为新的Excel表格
+df = pd.DataFrame(columns=['source ID'] + id_list)
+
+# 创建空行，所有technique_id种类的列都填充为0
+empty_row = {column: 0 for column in id_list}
+
+# 遍历JSON数据中的每个group和对应的technique_id列表
+for group, techniques in json_data.items():
+    # 复制空行，用作当前group的一行数据
+    row = empty_row.copy()
+    row['source ID'] = group
+
+    # 在每个technique_id种类列中填充1
+    for technique in techniques:
+        if technique in id_list:
+            row[technique] = 1
+
+    # 将当前行添加到结果DataFrame中
+    df = df.append(row, ignore_index=True)
+# 将结果保存到Excel表格
+df.to_excel('./DataSource/result.xlsx', index=False)
