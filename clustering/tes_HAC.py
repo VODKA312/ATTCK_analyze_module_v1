e4fad44eb4f93c90805fa6aba020aa0d3c65236a
VODKA312 <15356203871@163.com>
2023-08-14 16:45:28 +1000

    reproduce experiments

--- clustering/tes_HAC.py
+++ clustering/tes_HAC.py
@@ -9,30 +9,36 @@ data = pd.read_excel("../DataSource/result.xlsx")
 # 去除 'source ID' 列
 data = data.drop('source ID', axis=1)
 data_array = data.values
-# 计算样本之间的Phi系数距离矩阵
-def phi_coefficient_distance(X):
-    # 将NumPy数组转换为Pandas DataFrame对象
-    X_df = pd.DataFrame(X, columns=data.columns)
-    phi_matrix = phik_matrix(X_df.select_dtypes(include=['int64', 'bool']))  # 选择合适的数据类型进行计算
-    return phi_matrix
-# 初始化一个空的距离矩阵
-distance_matrix = np.zeros((data_array.shape[0], data_array.shape[0]))
+# 将DataFrame转换为NumPy数组
+data_array = data.values
 
-# 分批计算样本之间的Phi系数距离矩阵
+# 定义每个批次的大小
 batch_size = 75
-for i in range(0, data_array.shape[0], batch_size):
+
+# 计算距离矩阵的维度
+num_samples = data_array.shape[0]
+
+# 初始化一个空的距离矩阵
+distance_matrix = np.zeros((num_samples, num_samples))
+
+# 分批计算距离矩阵
+for i in range(0, num_samples, batch_size):
     start_idx = i
-    end_idx = min(i + batch_size, data_array.shape[0])
+    end_idx = min(i + batch_size, num_samples)
 
-    # 计算当前批次内样本之间的Phi系数距离
-    batch_distance = phi_coefficient_distance(data_array[start_idx:end_idx])
+    # 计算当前批次内样本之间的Jaccard距离
+    batch_distance = pairwise_distances(data_array[start_idx:end_idx], metric='jaccard')
 
-    # 将批次内的距离矩阵转换成一维形式，再填充到整体距离矩阵中
-    distance_matrix.reshape[-1,1] = squareform(batch_distance)
+    # 将当前批次内的距离矩阵填充到整体距离矩阵中
+    distance_matrix[start_idx:end_idx, start_idx:end_idx] = batch_distance
+
+# 将Jaccard距离转换为欧氏距离，以便与Ward链接法兼容
+euclidean_distance = squareform(pairwise_distances(data_array, metric='jaccard'))
+
+# 将欧氏距离数组转换成2D数组
+euclidean_distance = euclidean_distance.reshape(-1, 1)
 
 # 使用Ward链接法进行聚类
 n_clusters = 16
-cluster = AgglomerativeClustering(n_clusters=n_clusters, affinity='precomputed', linkage='ward')
-cluster_labels = cluster.fit_predict(distance_matrix)
-
-print(cluster_labels)
+cluster = AgglomerativeClustering(n_clusters=n_clusters, affinity='euclidean', linkage='ward')
+cluster_labels = cluster.fit_predict(euclidean_distance)
